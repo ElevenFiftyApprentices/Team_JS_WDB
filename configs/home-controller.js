1ec3ca558f6c94869ac623e4eb966076072db43f
King James <King James>
2017-03-10 10:03:04 -0500

    changed to saved mean template

--- configs/home-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp')
-
-//home controller - we will also use the data from shoppinglistitems service
-.controller('HomeController', ['$scope', 'ShoppingListItems', function($scope, ShoppingListItems) {
-	var home = $scope;
-	home.items = ShoppingListItems.getList();
-}]);
--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-//require my dependencies
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+'use strict';
 
-//next I need to make static files available
-app.use(express.static(__dirname));
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
-
-http.listen(process.env.PORT || 3000, function(){
-	console.log("listneing on port 3000 mane");
-});
+/**
+ * Module dependencies.
+ */
+var app = require('./config/lib/app');
+var server = app.start();
